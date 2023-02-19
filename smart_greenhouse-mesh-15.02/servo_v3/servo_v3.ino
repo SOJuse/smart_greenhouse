ac89eeb8e232de0d4990c26c15ac6cb4e959a046
SokurME <sokurme@gmail.com>
2023-02-19 23:29:24 +0300

    добавлена плата сервопривода

---
+++ smart_greenhouse-mesh-15.02/servo_v3/servo_v3.ino
@@ -0,0 +1,108 @@
+#include <Servo.h>
+#include <Wire.h>               // Подключаем библиотеку Wire 
+#include <Adafruit_INA219.h>    // Подключаем библиотеку Adafruit INA219 (A4-SDA-оранж.  A5-SCL-красн.)
+#define SERVO1_LEFT 12 
+#define SERVO2_RIGHT 11 
+#define CURRENT_SET 850 
+
+
+// создаём объекты для управления сервоприводами
+Servo myservo1;
+Servo myservo2;
+
+
+boolean start_up = false;
+boolean start_down = false;
+int angle = 0;
+String strData = "";
+boolean recievedFlag = false;
+Adafruit_INA219 ina219;         // Создаем объект ina219
+float current_mA = 0;       // Ток в мА 
+ 
+void setup() 
+{
+  // подключаем сервоприводы к выводам 11 и 12
+  myservo1.attach(SERVO1_LEFT);
+  myservo2.attach(SERVO2_RIGHT);
+  myservo1.write(0);
+  myservo2.write(180);
+   
+  delay(1000);
+  Serial.begin(9600); // подключаем монитор порта
+
+  if (! ina219.begin()) {       
+    Serial.println("Failed to find INA219 chip");  
+    }
+} 
+
+void window_move_up(int angle){
+// поднимаем вверх
+  Serial.println("Up!");
+  Serial.println(angle);
+  
+  for (byte i = 0; i <= angle; i++) {
+    myservo1.write(0 + i);
+    myservo2.write(180 - i);
+   // current_mA = ina219.getCurrent_mA();
+  //  if (current_mA > CURRENT_SET){ // ПЕРЕГРУЗКА!
+  //    break;
+   // }
+    Serial.print("i="); Serial.println(i);
+    Serial.print("cur="); Serial.println(current_mA);  
+    delay(50);
+  }
+}
+
+void window_move_down(int angle){
+// опускаем вниз
+  Serial.println("Down!");
+  Serial.println(angle);
+  
+  for (byte i = 0; i <= angle; i++) {
+    myservo1.write(angle - i);
+    myservo2.write(180 - angle + i);
+    Serial.print("i="); Serial.println(i);
+    delay(50);
+  }
+  
+}
+
+void loop() 
+{
+
+if (Serial.available() > 0) {                 // если есть что-то на вход
+    strData = "";                               // очистить строку
+    while (Serial.available() > 0) {            // пока идут данные
+      strData += (char)Serial.read();           // получаем данные
+      delay(2);                                 // обязательно задержка, иначе вылетим из цикла раньше времени
+    }
+    recievedFlag = true;                        // поднять флаг что получили данные
+  }
+  if (recievedFlag) {                           // если есть принятые данные
+    if (strData.startsWith("up")){               // разбор строки
+      start_up = true;
+      Serial.println("up");
+      Serial.println(start_up);
+    }
+    else if (strData.startsWith("down")){               // разбор строки
+      start_down = true;
+    }
+    else {
+      angle = strData.toInt();
+        }
+    recievedFlag = false;                       // данные приняты!
+  }
+
+
+//Serial.println(start_stop);
+if (start_up) {
+   window_move_up(angle);
+   start_up = false;
+}
+
+if (start_down) {
+   window_move_down(angle);
+   start_down = false;
+   }
+
+}
